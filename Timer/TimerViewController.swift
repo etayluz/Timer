3b444bf017a947d1ae053b824dff73a37682742b
Etay Luz <etayluz@gmail.com>
2015-11-06 16:51:18 -0500

    Get timer to work right

--- Timer/TimerViewController.swift
+++ Timer/TimerViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+enum Component: Int {
+  case Hours = 0, Minutes, Seconds
+}
+
+
 class TimerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate  {
   @IBOutlet var timerPickerView: UIPickerView!
+  @IBOutlet var timerButton: UIButton!
   
+  var timer = NSTimer()
   let timerPickerData = [Array(1...24), Array(1...60), Array(1...60)]
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -35,6 +42,64 @@ class TimerViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
     let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "SlimJoe", size: 30.0)!,NSForegroundColorAttributeName:UIColor.whiteColor()])
     return myTitle
   }
+  
+  
+//  func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView{
+//    var pickerLabel = view as? UILabel;
+//  
+//    if (pickerLabel == nil) {
+//      pickerLabel = UILabel()
+//      pickerLabel?.font = UIFont(name: "SlimJoe1", size: 30.0)
+//      pickerLabel?.textColor = UIColor.whiteColor()
+//      pickerLabel?.textAlignment = NSTextAlignment.Center
+//    }
+//
+//    pickerLabel?.text = String(format:"%02d", timerPickerData[component][row])
+//    return pickerLabel!;
+//  }
+  
+  //MARK: - Actions
+  @IBAction func timerButtonPressed(sender: UIButton) {
+    if timerButton.titleLabel?.text == "START" {
+      timerButton.setTitle("STOP", forState: UIControlState.Normal)
+      timer = NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector: "countDown", userInfo: nil, repeats: true)
+      NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+    }
+    else {
+      timerButton.setTitle("START", forState: UIControlState.Normal)
+      timer.invalidate()
+    }
+  }
+  
+  func countDown() {
+    var secondsSelectedRow = timerPickerView.selectedRowInComponent(Component.Seconds.rawValue)
+    var minutesSelectedRow = timerPickerView.selectedRowInComponent(Component.Minutes.rawValue)
+    var hoursSelectedRow = timerPickerView.selectedRowInComponent(Component.Hours.rawValue)
+
+    secondsSelectedRow--
+    
+    if (secondsSelectedRow < 0)
+    {
+      secondsSelectedRow = timerPickerData[Component.Seconds.rawValue].count - 1
+      minutesSelectedRow--
+    }
+    if (minutesSelectedRow < 0)
+    {
+      minutesSelectedRow = timerPickerData[Component.Minutes.rawValue].count - 1
+      hoursSelectedRow--
+    }
+    if (hoursSelectedRow < 0)
+    {
+      hoursSelectedRow = timerPickerData[Component.Hours.rawValue].count - 1
+    }
+    
+    self.timerPickerView.selectRow(secondsSelectedRow, inComponent: Component.Seconds.rawValue, animated: false)
+    self.timerPickerView.selectRow(minutesSelectedRow, inComponent: Component.Minutes.rawValue, animated: false)
+    self.timerPickerView.selectRow(hoursSelectedRow, inComponent: Component.Hours.rawValue, animated: false)
+    
+  }
+  
+  
 
 }
 
